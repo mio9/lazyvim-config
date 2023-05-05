ce667eb4d11607587ec6b5804f3ba0e839b1d6ed
Mio Nekoya <admin@mio9.sh>
2023-05-05 21:24:23 +0900

    Add outline and comment toggle

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+  {
+    "simrat39/symbols-outline.nvim",
+    config = function()
+      require("symbols-outline").setup()
+    end,
+  },
+}
