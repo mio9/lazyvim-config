6780138d9a95d8dd8bba8193100fc33b5e8241b3
Mio Nekoya <admin@mio9.sh>
2025-08-23 07:54:42 +0900

    Fix bufferline issue with catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,17 @@ return {
     "catppuccin/nvim",
     lazy = true,
     name = "catppuccin",
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.groups.integrations.bufferline").get_theme()
+          end
+        end,
+      },
+    },
   },
   {
     "navarasu/onedark.nvim",
