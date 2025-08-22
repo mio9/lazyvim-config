4be6679ec9b27b7a148199eb6835c40d380616fa
Mio Nekoya <admin@mio9.sh>
2025-08-23 07:38:13 +0900

    add folke trouble

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -14,5 +14,10 @@ return {
   {
     "fedepujol/move.nvim",
   },
-  { "akinsho/toggleterm.nvim", version = "*", config = true }
+  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  }
 }
